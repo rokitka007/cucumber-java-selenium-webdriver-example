870e02cf46d860cfc41d2b12bf1a4b412214257b
rokitka007 <k.popielarczyk91@gmail.com>
2019-09-22 08:15:38 +0200

    Added firefox browser

--- src/test/java/com/automatedtest/sample/infrastructure/driver/Setup.java
+++ src/test/java/com/automatedtest/sample/infrastructure/driver/Setup.java
@@ -5,20 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Setup {
 
     public static WebDriver driver;
-    private static String chromePath = "" +
-            "src/webdriver/chromedriver.exe";
+    private static String chromePath = "src/webdriver/chromedriver.exe";
+    private static String firefoxPath = "src/webdriver/geckodriver.exe";
+
 
     @Before
     public void setWebDriver() throws Exception {
 
         String browser = System.getProperty("browser");
         if (browser == null) {
-            browser = "chrome";
+            browser = "firefox";
         }
+
         switch (browser) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", chromePath);
@@ -27,8 +30,10 @@ public class Setup {
                 driver = new ChromeDriver(chromeOptions);
                 break;
             case "firefox":
+                System.setProperty("webdriver.gecko.driver", firefoxPath);
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("start-maximized");
                 driver = new FirefoxDriver();
-                driver.manage().window().maximize();
                 break;
             default:
                 throw new IllegalArgumentException("Browser \"" + browser + "\" isn't supported.");
--- src/test/resources/com/automatedtest/sample/Search.feature
+++ src/test/resources/com/automatedtest/sample/Search.feature
@@ -1,12 +1,27 @@
 Feature: Search
 
+  Background: db
+    Given A user navigates to HomePage
 
   @search @search_01
   Scenario Outline: Search Booking.com
-    Given A user navigates to HomePage
     When a user searches for "Gdańsk"
-    Then "Little Home - Kontinuum" is displayed in the first "<nbOfResultsToSearch>" results
+    Then "Apartament Kapitański" is displayed in the first "<nbOfResultsToSearch>" results
 
     Examples:
       | nbOfResultsToSearch |
       | 5 |
+
+
+  @search @search_02
+  Scenario Outline: Search "<miasto>" in Booking.com
+    When a user searches for "<miasto>"
+    Then "<oferta>" is displayed in the first "<nbOfResultsToSearch>" results
+
+    Examples:
+      | miasto | oferta | nbOfResultsToSearch |
+      | Kraków | B&B Hotel Krakow Centrum | 4 |
+      | Malbork | U Marzeny | 5 |
+      | Warszawa |Royal Park Apartment | 5 |
+
+
